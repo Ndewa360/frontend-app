04fb1f280f48d8a7c2759d0c33968c879053dabd
Cedric-Yaba-In <c.nguendap@yaba-in.com>
2024-10-24 15:32:32 +0100

    update /env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-  apiUrl: 'https://api.ndewa360.com',
-	url: 'https://ndewa360.com',
+  apiUrl: 'https://api.ndewa-360.com',
+	url: 'https://ndewa-360.com',
   }
   
