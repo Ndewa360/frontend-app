eb1d5a91b143f3ab231f5fc8e8e6efc761bf9dd5
Cedric-Yaba-In <c.nguendap@yaba-in.com>
2025-10-27 09:08:36 +0100

    translate welcome

--- src/app/main/welcome/welcome.component.ts
+++ src/app/main/welcome/welcome.component.ts
@@ -11,6 +11,15 @@ import { UserProfileModel, UserProfileState } from 'src/app/shared/store';
 })
 export class WelcomeComponent implements OnInit {
   @Select(UserProfileState.selectStateUserProfile) userProfile$:Observable<UserProfileModel>
+  
+  guides = [
+    { key: 'INTRODUCTION', durationKey: 'INTRODUCTION' },
+    { key: 'ADVANCED_TOPICS', durationKey: 'ADVANCED_TOPICS' },
+    { key: 'FINANCIAL_MANAGEMENT', durationKey: 'FINANCIAL_MANAGEMENT' },
+    { key: 'LISTING_MANAGEMENT', durationKey: 'LISTING_MANAGEMENT' },
+    { key: 'ADMINISTRATION', durationKey: 'ADMINISTRATION' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
